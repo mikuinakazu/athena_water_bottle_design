a85d7161485e5ff83958adf1c702b48c01d1158d
稲員未来 <miku.inakazu@gmail.com>
2018-05-02 20:34:39 +0900

    make design model and associated with user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180502112337) do
+ActiveRecord::Schema.define(version: 20180502113129) do
 
   create_table "designs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "image"
