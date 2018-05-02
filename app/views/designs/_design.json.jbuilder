facf4d28c97dd7c3ba517b25414a4f00ba7f8c15
稲員未来 <miku.inakazu@gmail.com>
2018-05-02 20:28:28 +0900

    delete scaffold

--- app/views/designs/_design.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! design, :id, :image, :comment, :user_id, :created_at, :updated_at
-json.url design_url(design, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root "designs#index"
-  resources :designs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
