f8da6bd4edd9acff5b68bd988949b2c190321c0f
稲員未来 <miku.inakazu@gmail.com>
2018-05-06 11:25:59 +0900

    add 4.2

--- db/migrate/20180506020155_add_attachment_image_to_designs.rb
+++ db/migrate/20180506020155_add_attachment_image_to_designs.rb
@@ -1,4 +1,4 @@
-class AddAttachmentImageToDesigns < ActiveRecord::Migration
+class AddAttachmentImageToDesigns < ActiveRecord::Migration[4.2]
   def self.up
     change_table :designs do |t|
       t.attachment :image
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180502113129) do
+ActiveRecord::Schema.define(version: 20180506020155) do
 
   create_table "designs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text "comment"
     t.integer "user_id"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
